0be37a234c3b310b307cd50c7b4a47cdf178528d
sokol <sokolovagb@gmail.com>
2025-06-19 19:51:02 +0300

    KTB-16 sendMenu trainer

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -3,36 +3,35 @@ package org.example
 fun main(args: Array<String>) {
 
     val botToken = args[0]
+    var updateId: Int = 0
+
     val telegramBotService = TelegramBotService(botToken)
+
     val updateIdRegex: Regex = "\"update_id\":(.+?),\n\"message\":".toRegex()
     val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
     val chatIdRegex: Regex = """},"chat":\{"id":(\d+),"first_name"""".toRegex()
-    var updateId: Int = 0
+    val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
+
+    val trainer = LearnWordsTrainer()
 
     while (true) {
         Thread.sleep(2000)
         val updates: String = telegramBotService.getUpdates(updateId)
         println(updates)
-        val updateIdMatchResult: MatchResult? = updateIdRegex.find(updates)
-        val updateIdMatchGroup: MatchGroupCollection? = updateIdMatchResult?.groups
-        updateId = updateIdMatchGroup?.get(1)?.value?.toInt()?.plus(1) ?: continue
+        updateId = updateIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull()?.plus(1) ?: continue
+        val text = messageTextRegex.find(updates)?.groups?.get(1)?.value
+        val chatId = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull() ?: continue
+        val data = dataRegex.find(updates)?.groups?.get(1)?.value
 
-        val textMatchResult: MatchResult? = messageTextRegex.find(updates)
-        val textMatchGroup: MatchGroupCollection? = textMatchResult?.groups
-        val text = textMatchGroup?.get(1)?.value
-
-        val chatIdMatchResult: MatchResult? = chatIdRegex.find(updates)
-        val chatIdMatchGroup: MatchGroupCollection? = chatIdMatchResult?.groups
-        val chatId = chatIdMatchGroup?.get(1)?.value?.toIntOrNull() ?: continue
-
-        if (text != null) {
-            if (text.length in MINIMUM_NUMBER_OF_CHARACTERS_IN_TEXT..MAXIMUM_NUMBER_OF_CHARACTERS_IN_TEXT) {
-                telegramBotService.sendMessage(chatId, text)
-            }
+        if (text?.lowercase() == "hello") {
+            telegramBotService.sendMessage(chatId, text)
+        }
+        if (text?.lowercase() == "/start") {
+            telegramBotService.sendMenu(chatId)
+        }
+        if (data?.lowercase() == "statistics_clicked") {
+            telegramBotService.sendMessage(chatId, "Выучено 10 из 10 слов | 100%")
         }
     }
 
 }
-
-const val MINIMUM_NUMBER_OF_CHARACTERS_IN_TEXT = 1
-const val MAXIMUM_NUMBER_OF_CHARACTERS_IN_TEXT = 4096
